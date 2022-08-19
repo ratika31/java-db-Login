5fddd259c0db5bd2b9d2fe83d5f1b587f44099b7
ratika <ratika.rathi@gmail.com>
2022-08-19 15:53:22 +0530

    database config changed

--- src/main/java/com/example/DbUtil.java
+++ src/main/java/com/example/DbUtil.java
@@ -9,7 +9,7 @@ import java.sql.SQLException;
 public class DbUtil {
 	static Connection con=null;
 	static String driver = "com.mysql.cj.jdbc.Driver";
-	static String url = "jdbc:mysql://homeloan.ctsskph1rwug.ap-south-1.rds.amazonaws.com:3306/homeloan";
+	static String url = "jdbc:mysql://lwplabs.cnouzhrfanwt.us-east-1.rds.amazonaws.com:3306/homeloan";
 	static String username = "master";
 	static String password = "Welcome123";
 	
